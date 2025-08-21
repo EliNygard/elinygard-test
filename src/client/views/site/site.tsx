c9656731b35026d8e8d370dce231e185004da46d
EliNygard <eli.nygard@gmail.com>
2025-08-21 15:59:12 +0200

    Builds and styles site details page

--- src/client/views/site/site.tsx
+++
@@ -1,9 +0,0 @@
-import { Heading, Page } from "@oliasoft-open-source/react-ui-library";
-
-export const SitePage = ({}) => {
-  return (
-    <Page left={0}>
-      <Heading top>Site Page</Heading>
-    </Page>
-  );
-};
