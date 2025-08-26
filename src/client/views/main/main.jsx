8bf7ed4eedbc93d8751a6013d84f5425f1ea5821
EliNygard <eli.nygard@gmail.com>
2025-08-26 12:40:20 +0200

    Corrects heading text content

--- src/client/views/main/main.jsx
+++ src/client/views/main/main.jsx
@@ -5,7 +5,7 @@ import { Sites } from "client/components/sites/sites";
 export const Main = ({}) => {
   return (
     <Page left={0}>
-      <Heading top>Oil Rigs</Heading>
+      <Heading top>Oil Sites</Heading>
       <Spacer />
       <Sites />
     </Page>
