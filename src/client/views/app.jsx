627cdafd8c0e25ebe1e5c0df90adc90d28e3b93a
EliNygard <eli.nygard@gmail.com>
2025-08-26 12:27:43 +0200

    Extends TopBar with navigation links, adds new routes

--- src/client/views/app.jsx
+++ src/client/views/app.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { TopBar } from "@oliasoft-open-source/react-ui-library";
 import Logo from "client/views/images/logo.svg";
-
 import { Main } from "client/views/main/main";
-import { SitePage } from "./site/site";
+import { SitePage } from "./site-page/site";
+import { RigsPage } from "./oil-rigs/oil-rigs";
+import { ChartPage } from "./chart/chart";
 
 export const App = () => {
   return (
@@ -13,9 +13,31 @@ export const App = () => {
         title={{
           logo: <img src={Logo} alt="logo" style={{ height: 28 }} />,
         }}
+        content={[
+          {
+            type: "Link",
+            label: "Oil Sites",
+            component: Link,
+            url: "/",
+          },
+          {
+            type: "Link",
+            label: "Oil Rigs",
+            component: Link,
+            url: "/oil-rigs",
+          },
+          {
+            type: "Link",
+            label: "Chart",
+            component: Link,
+            url: "/chart",
+          },
+        ]}
       />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/oil-rigs" element={<RigsPage />} />
+        <Route path="/chart" element={<ChartPage />} />
         <Route path="site/:id" element={<SitePage />} />
       </Routes>
     </>
