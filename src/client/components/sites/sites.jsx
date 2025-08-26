5b9a3488d00ed92c08bb53bbdf07665278c2422f
EliNygard <eli.nygard@gmail.com>
2025-08-26 15:35:16 +0200

    Adds sort by name to site list

--- src/client/components/sites/sites.jsx
+++ src/client/components/sites/sites.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import {
@@ -8,11 +8,37 @@ import {
   Column,
   Row,
   Accordion,
+  Select,
 } from "@oliasoft-open-source/react-ui-library";
 import { sitesLoaded } from "store/entities/sites/sites";
 import styles from "./sites.module.less";
 
 const Sites = ({ list, loading, sitesLoaded }) => {
+  const [sortOrder, setSortOrder] = useState("none");
+
+  const handleSortChange = (event, ...rest) => {
+    console.log("event.target.value:", event.target.value);
+    const value = event.target.value;
+    if (value === "asc" || value === "desc" || value === "none") {
+      setSortOrder(value);
+    }
+  };
+
+  const sortedList = useMemo(() => {
+    if (!Array.isArray(list)) return [];
+    if (sortOrder === "none") return list;
+
+    const copyList = [...list];
+
+    copyList.sort((a, b) =>
+      (a.name ?? "").localeCompare(b.name ?? "", "nb", { sensitivity: "base" })
+    );
+    if (sortOrder === "desc") copyList.reverse();
+    console.log(copyList);
+
+    return copyList;
+  }, [list, sortOrder]);
+
   return (
     <Card heading={<Heading>List of oil sites</Heading>}>
       <Row>
@@ -23,12 +49,30 @@ const Sites = ({ list, loading, sitesLoaded }) => {
             loading={loading}
             disabled={loading}
           />
+          <Select
+            native
+            onChange={handleSortChange}
+            options={[
+              {
+                label: "Sort by name",
+                type: "Heading",
+              },
+              {
+                label: "A-Z",
+                value: "asc",
+              },
+              {
+                label: "Z-A",
+                value: "desc",
+              },
+            ]}
+          />
         </Column>
         <Column>
           <div className={styles.sitesList}>
-            {list.length ? (
+            {sortedList.length ? (
               <ul>
-                {list.map((site, i) => (
+                {sortedList.map((site, i) => (
                   <li key={i}>
                     <Card heading={`Name: ${site.name}`}>
                       <Link to={`/site/${site.id}`}>
